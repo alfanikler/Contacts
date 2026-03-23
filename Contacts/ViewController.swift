e6c5cc71af56a253e5c0f471ea2af066aaa6201c
Petrov Nikita <npetrov3@alfabank.ru>
2026-03-23 19:11:16 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Alfa on 23.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
