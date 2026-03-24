e3c09f5834441a4f1929c58f74ece3db62021c92
alfanikler <alfanikler@gmail.com>
2026-03-24 12:42:22 +0300

    micro refactoring for tests

--- ContactsTests/ContactStorageTests.swift
+++ ContactsTests/ContactStorageTests.swift
@@ -9,19 +9,19 @@ import XCTest
 @testable import Contacts
 
 final class MockContactStore: ContactStoreProtocol {
-    typealias SetMethodCallsItem = (array: [[String: String]], defaultName: String)
+    typealias SetMethodArgs = (array: [[String: String]], defaultName: String)
     
-    var arrayMethodCalls = [String]()
-    var setMethodCalls = [SetMethodCallsItem]()
+    var callsToArrayMethod = [String]()
+    var callsToSetMethod = [SetMethodArgs]()
     
     func array(forKey defaultName: String) -> [Any]? {
-        arrayMethodCalls.append(defaultName)
+        callsToArrayMethod.append(defaultName)
         return []
     }
 
     func set(_ array: [Any]?, forKey defaultName: String) {
-        let call = (array: array as! [[String: String]], defaultName: defaultName)
-        setMethodCalls.append(call)
+        let call = (array: array, defaultName: defaultName) as! SetMethodArgs
+        callsToSetMethod.append(call)
     }
 }
 
@@ -35,14 +35,14 @@ final class ContactsStorageTests: XCTestCase {
         let testContact2 = Contact(title: "Test2", phone: "Test2")
         
         var contacts = [Contact]()
-        var calls = [MockContactStore.SetMethodCallsItem]()
+        var calls = [MockContactStore.SetMethodArgs]()
         
         XCTAssertEqual(calls.count, 0)
         
         // Добавляем первый контакт
         contacts.append(testContact1)
         contactStorage.save(contacts: contacts)
-        calls = mockContactStore.setMethodCalls
+        calls = mockContactStore.callsToSetMethod
         
         XCTAssertEqual(calls.count, 1)
         XCTAssertEqual(calls.last?.array[0], ["title": testContact1.title, "phone": testContact1.phone])
@@ -51,7 +51,7 @@ final class ContactsStorageTests: XCTestCase {
         // Добавляем второй контакт
         contacts.append(testContact2)
         contactStorage.save(contacts: contacts)
-        calls = mockContactStore.setMethodCalls
+        calls = mockContactStore.callsToSetMethod
         
         XCTAssertEqual(calls.count, 2)
         XCTAssertEqual(calls.last?.array[0], ["title": testContact1.title, "phone": testContact1.phone])
@@ -61,21 +61,21 @@ final class ContactsStorageTests: XCTestCase {
         // Удаляем контакты
         contacts.removeAll()
         contactStorage.save(contacts: contacts)
-        calls = mockContactStore.setMethodCalls
+        calls = mockContactStore.callsToSetMethod
         
         XCTAssertEqual(calls.count, 3)
         XCTAssertEqual(calls.last?.array, [])
         XCTAssertEqual(calls.last?.defaultName, "contacts")
         
         // Проверяем получение контактов
-        XCTAssertEqual(mockContactStore.arrayMethodCalls.count, 0)
+        XCTAssertEqual(mockContactStore.callsToArrayMethod.count, 0)
 
         _ = contactStorage.load()
         
-        XCTAssertEqual(mockContactStore.arrayMethodCalls.count, 1)
+        XCTAssertEqual(mockContactStore.callsToArrayMethod.count, 1)
         
         _ = contactStorage.load()
         
-        XCTAssertEqual(mockContactStore.arrayMethodCalls.count, 2)
+        XCTAssertEqual(mockContactStore.callsToArrayMethod.count, 2)
     }
 }
